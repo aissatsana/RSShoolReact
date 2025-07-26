874f31c5a9aa4ce0cb57fd6a9e9c206918fd9ee2
Анастасия <nastya.miss.7@gmail.com>
2025-07-26 21:50:45 +0300

    fix: change class component loader to functional

--- class-components/src/components/Loader/Loader.tsx
+++ class-components/src/components/Loader/Loader.tsx
@@ -1,15 +1,8 @@
-import { Component, type ReactNode } from 'react';
 import './style.css';
 
-export class Loader extends Component {
-  render(): ReactNode {
-    return (
-      <>
-        <div className="loader">
-          <div className="loader__spinner"></div>
-          <p className="loader__text">Loading...</p>
-        </div>
-      </>
-    );
-  }
-}
+export const Loader = () => (
+  <div className="loader">
+    <div className="loader__spinner"></div>
+    <p className="loader__text">Loading...</p>
+  </div>
+);
