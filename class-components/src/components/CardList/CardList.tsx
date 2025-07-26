5fb5361fb4bbc42260d8981826eda8c074f480dd
Анастасия <nastya.miss.7@gmail.com>
2025-07-26 21:48:38 +0300

    fix: change class component cardList to functional

--- class-components/src/components/CardList/CardList.tsx
+++ class-components/src/components/CardList/CardList.tsx
@@ -1,4 +1,4 @@
-import { Component, type ReactNode } from 'react';
+import { type FC } from 'react';
 import type { Character } from '../../types';
 import { Card } from '../Card';
 import './style.css';
@@ -7,21 +7,18 @@ interface CardListProps {
   items?: Character[];
 }
 
-export class CardList extends Component<CardListProps> {
-  render(): ReactNode {
-    const { items } = this.props;
-
-    if (!items || items.length === 0) {
-      return <p>No characters found</p>;
-    }
-    return (
-      <ul className="list">
-        {items.map((item) => (
-          <li className="list__item" key={item.id}>
-            <Card item={item} />
-          </li>
-        ))}
-      </ul>
-    );
+export const CardList: FC<CardListProps> = ({ items }) => {
+  if (!items || items.length === 0) {
+    return <p>No characters found</p>;
   }
-}
+
+  return (
+    <ul className="list">
+      {items.map((item) => (
+        <li className="list__item" key={item.id}>
+          <Card item={item} />
+        </li>
+      ))}
+    </ul>
+  );
+};
